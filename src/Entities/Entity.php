896fb217f12ebc49f89e0d0f640447015c3d6f33
Yuriy Martini <hi@ymartini.me>
2023-02-12 18:06:54 +0100

    Revert "Dev"

--- src/Entities/Entity.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Condividendo\LaravelCBI\Entities;
-
-use Condividendo\LaravelCBI\Contracts\Entity as EntityContract;
-
-abstract class Entity implements EntityContract
-{
-    final public function __construct() { }
-}
